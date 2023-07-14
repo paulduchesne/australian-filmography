5c4d62b0156edc879a5cc4469d0e1639a63762ff
paulduchesne <paulduchesne@tuta.io>
2023-07-14 21:38:47 +1000

    fetch wikidata statements

---
+++ pull.py
@@ -0,0 +1,20 @@
+# pull down turtle from Wikidata.
+
+import json
+import pathlib
+import pydash
+import requests
+import tqdm
+
+with open(pathlib.Path.cwd() / 'corpus.json') as corpus:
+    corpus = pydash.get(json.load(corpus), 'works')
+
+for x in tqdm.tqdm(corpus):
+    path = f"https://www.wikidata.org/wiki/Special:EntityData?id={x['wikidata_id']}&format=ttl"
+    response = requests.get(path)
+    if response.status_code == 200:
+        local = pathlib.Path.cwd() / 'data' / f"{x['wikidata_id']}.ttl"
+        with open(local, 'w') as export:
+            export.write(response.text)
+    else:
+        raise Exception(f"Cound not fetch {x['wikidata_id']}.")
