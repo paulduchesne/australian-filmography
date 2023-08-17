4ee2f6f0b01d819eede7e0c29270e25b27981c9d
Paul Duchesne <45246635+paulduchesne@users.noreply.github.com>
2023-08-17 20:50:40 +1000

    pull simple Wikidata

--- pull.py
+++ pull.py
@@ -10,7 +10,7 @@ with open(pathlib.Path.cwd() / 'corpus.json') as corpus:
     corpus = pydash.get(json.load(corpus), 'works')
 
 for x in tqdm.tqdm(corpus):
-    path = f"https://www.wikidata.org/wiki/Special:EntityData?id={x['wikidata_id']}&format=ttl"
+    path = f"https://www.wikidata.org/wiki/Special:EntityData/{x['wikidata_id']}.ttl?flavor=simple"
     response = requests.get(path)
     if response.status_code == 200:
         local = pathlib.Path.cwd() / 'data' / f"{x['wikidata_id']}.ttl"
