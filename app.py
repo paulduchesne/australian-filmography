51f498d56e6b1a6a07d7a953c6dd47dab13e8b30
paulduchesne <paulduchesne@tuta.io>
2026-04-14 07:41:00 +1000

    viable resources for entity page

--- app.py
+++ app.py
@@ -65,6 +65,80 @@ def home_page():
 
     return render_template('index.html', data=json.loads(json.dumps(submit_data)))
 
+@app.route('/film/<entity>', methods=['GET', 'POST'])
+def entity_page(entity):
+
+    query = '''
+        prefix wd: <http://www.wikidata.org/entity/>
+        prefix wpd: <http://www.wikidata.org/prop/direct/>
+        select distinct (str(?filmLabel) as ?lab) (YEAR(?date) AS ?year)
+            ?director ?directorLabel ?writer ?writerLabel
+            ?dop ?dopLabel ?editor ?editorLabel
+            ?composer ?composerLabel ?producer ?producerLabel ?cast ?castLabel
+            ?genre ?genreLabel ?rating ?ratingLabel
+            ?colour ?colourLabel ?aspect ?aspectLabel
+        where {
+            values ?film { wd:'''+entity+''' }
+            ?film wpd:P31 wd:Q11424.
+            ?film rdfs:label ?filmLabel .
+            ?film wpd:P577 ?date .
+            ?film wpd:P57 ?director .
+            ?director rdfs:label ?directorLabel .
+            optional { ?film wpd:P58 ?writer . ?writer rdfs:label ?writerLabel . }
+            optional { ?film wpd:P344 ?dop . ?dop rdfs:label ?dopLabel . }
+            optional { ?film wpd:P1040 ?editor . ?editor rdfs:label ?editorLabel . }
+            optional { ?film wpd:P86 ?composer . ?composer rdfs:label ?composerLabel . }
+            optional { ?film wpd:P162 ?producer . ?producer rdfs:label ?producerLabel . }
+            optional { ?film wpd:P161 ?cast . ?cast rdfs:label ?castLabel . }
+            optional { ?film wpd:P136 ?genre . ?genre rdfs:label ?genreLabel . }
+            optional { ?film wpd:P3156 ?rating . ?rating rdfs:label ?ratingLabel . }
+            optional { ?film wpd:P462 ?colour . ?colour rdfs:label ?colourLabel . }
+            optional { ?film wpd:P2061 ?aspect . ?aspect rdfs:label ?aspectLabel . }
+        }
+    '''
+
+    cols = ['label', 'year', 'director', 'directorLabel',
+            'writer', 'writerLabel', 'dop', 'dopLabel',
+            'editor', 'editorLabel', 'composer', 'composerLabel',
+            'producer', 'producerLabel', 'cast', 'castLabel', 'genre', 'genreLabel',
+            'rating', 'ratingLabel', 'colour', 'colourLabel',
+            'aspect', 'aspectLabel'
+    ]
+
+    results = g.query(query)
+    df = pandas.DataFrame(results, columns=cols)
+    for x in ['director', 'writer', 'dop', 'editor', 'composer', 'producer', 'cast', 'genre', 'rating', 'colour', 'aspect']:
+        df[x] = df[x].str.split('/').str[-1]
+
+    data = {
+        'label': df[['label']].drop_duplicates().to_dict('records'),
+        'year': df[['year']].drop_duplicates().to_dict('records'),
+        'info':[]
+    }
+
+
+    crew_array = list()
+    for x in ['cast']:
+        crew_array.append({'attribute':x, 'data': df[[x, f'{x}Label']].rename(columns={x:'link',f'{x}Label':'label'}).drop_duplicates().to_dict('records')})
+
+    data['info'].append({'section':'cast', 'payload':crew_array})
+
+    crew_array = list()
+    for x in ['director', 'writer', 'producer', 'dop', 'editor', 'composer']:
+        crew_array.append({'attribute':x, 'data': df[[x, f'{x}Label']].rename(columns={x:'link',f'{x}Label':'label'}).drop_duplicates().to_dict('records')})
+
+    data['info'].append({'section':'crew', 'payload':crew_array})
+
+    crew_array = list()
+    for x in ['genre', 'rating', 'colour', 'aspect']:
+        crew_array.append({'attribute':x, 'data': df[[x, f'{x}Label']].rename(columns={x:'link',f'{x}Label':'label'}).drop_duplicates().to_dict('records')})
+
+    data['info'].append({'section':'technical', 'payload':crew_array})
+
+    print(json.dumps(crew_array, indent=4))
+    print(json.dumps(data, indent=4))
+
+    return render_template('entity.html', data=data)
 
 
 if __name__ == "__main__":
