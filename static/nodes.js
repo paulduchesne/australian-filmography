a336b8f82bc2d1d219c12a8a132b306fe69bfd1f
paulduchesne <paulduchesne@tuta.io>
2026-04-15 16:52:57 +1000

    updated node link

--- static/nodes.js
+++ static/nodes.js
@@ -40,7 +40,7 @@ async function drawNodes(nodes) {
 
         })
         .on('click', function(k, d) {
-            window.location.href = 'http://localhost:5000/film/'+d['id'];
+            window.location.href = '/film/'+d['id'];
         })
 
     d3.select('#canvas')
