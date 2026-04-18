d5741b46514233f6d91308c07999c0595d532536
paulduchesne <paulduchesne@tuta.io>
2026-04-19 06:18:16 +1000

    latest data snapshot

--- wikidata.py
+++ wikidata.py
@@ -28,7 +28,7 @@ headers = {
     'Accept': 'application/json'
 }
 
-for wikidata_id in tqdm.tqdm(df.wikidata.unique()):
+for wikidata_id in tqdm.tqdm(df.wikidata.unique()[:700]):
     wikidata_id_hash = hashlib.md5(wikidata_id.encode()).hexdigest()
     json_path = pathlib.Path.cwd() / 'data' / f'{wikidata_id_hash}.json'
 
