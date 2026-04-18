5d4cccccfe0b078ab35b047525e0f95cd721e2e3
paulduchesne <paulduchesne@tuta.io>
2026-04-18 21:29:53 +1000

    introduced absolute links

--- static/nodes.js
+++ static/nodes.js
@@ -40,7 +40,7 @@ async function drawNodes(nodes) {
 
         })
         .on('click', function(k, d) {
-            window.location.href = 'film/'+d['id'];
+            window.location.href = 'https://paulduchesne.github.io/australian-filmography/film/'+d['id'];
         })
 
     d3.select('#canvas')
