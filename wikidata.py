0e4420a976cc9b5496b78c7e804351cf77ac1991
paulduchesne <paulduchesne@tuta.io>
2026-04-21 13:08:52 +1000

    added property queries

--- wikidata.py
+++ wikidata.py
@@ -34,7 +34,7 @@ for wikidata_id in tqdm.tqdm(df.wikidata.unique()):
 
     if not json_path.exists():
 
-        time.sleep(47)
+        time.sleep(17)
 
         with open(pathlib.Path.cwd() / 'wikidata.rq') as query:
             query = query.read().replace('FILM', wikidata_id)
@@ -56,10 +56,73 @@ for json_file in (pathlib.Path.cwd() / 'data').iterdir():
         json_data = json.load(json_data)
 
     aggregated_file += json_data
-    print(json_file.stem, len(json_data), type(json_data))
-    print(len(aggregated_file))
 
 with open(pathlib.Path.cwd() / 'wikidata.json', 'w') as aggregated_out:
     json.dump(aggregated_file, aggregated_out, ensure_ascii=False, indent=4)
 
 # TODO: it would be good to add a survey of available data points for each record.
+#
+
+print(len(aggregated_file))
+
+g = rdflib.Graph().parse('wikidata.json', format="json-ld")
+
+# derive a list of all films.
+
+query = '''
+    prefix au: <http://ausfilmography/>
+    prefix wd: <http://www.wikidata.org/entity/>
+    prefix wpd: <http://www.wikidata.org/prop/direct/>
+    select distinct ?film
+    where {
+        ?film wpd:P31 wd:Q11424
+        }
+    '''
+
+result = g.query(query)
+wikidata_ids = ['wd:'+pathlib.Path(x.film).stem for x in result]
+
+# derive a list of film properties.
+
+query = '''
+    prefix au: <http://ausfilmography/>
+    prefix wd: <http://www.wikidata.org/entity/>
+    prefix wpd: <http://www.wikidata.org/prop/direct/>
+    select distinct ?prop
+    where {
+        values ?film {'''+' '.join(wikidata_ids)+'''}
+        ?film ?prop ?value .
+        }
+    '''
+
+result = g.query(query)
+props = [x.prop for x in result]
+
+# report on coverage of each property.
+
+for p in sorted(props):
+
+    p = pathlib.Path(p).stem
+    if p[0] != 'P':
+        continue
+
+    query = '''
+        prefix au: <http://ausfilmography/>
+        prefix wd: <http://www.wikidata.org/entity/>
+        prefix wpd: <http://www.wikidata.org/prop/direct/>
+        select distinct ?film
+        where {
+            values ?prop { wpd:'''+p+''' }
+            ?film ?prop ?value .
+            }
+        '''
+
+    result = g.query(query)
+    matching_films = [x.film for x in result]
+    ratio = round(len(matching_films)/len(wikidata_ids), 2)
+    print(p, ratio)
+
+# TODO: this needs some further work,
+# likely due to not all films being "films", ie Q11424.
+# First pass should probably be to assess what are in scope entities
+# which we can do explicitly as we have source ids from the datasets.
